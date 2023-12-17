f9473a4e08457e360d53dfcfc7c4d2cafcc106a3
Gabriel Castro <gabriel.programador.ads@gmail.com>
2023-12-17 00:19:17 -0300

    att

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,6 @@
 <?php
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,5 @@ Route::get('/', 'FirstController@index');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/produtos', 'ProdutoControlador@index');
