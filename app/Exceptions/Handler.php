ae98c8e14dfaae86922a69bdb977adee14519b7b
Gabriel Castro <gabriel.programador.ads@gmail.com>
2023-12-18 13:45:06 -0300

    att

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Auth\AuthenticationException;
 
 class Handler extends ExceptionHandler
 {
@@ -34,6 +35,7 @@ class Handler extends ExceptionHandler
      */
     public function report(Exception $exception)
     {
+        // Llama al método report de la clase padre
         parent::report($exception);
     }
 
@@ -46,6 +48,31 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        // Llama al método render de la clase padre
         return parent::render($request, $exception);
+    }
+
+    ////////// Adicionar ====>>>>>>>>>>>>>>
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if ($request->expectsJson()) {
+            // Si la solicitud espera una respuesta JSON
+            return response()->json(['message' => $exception->getMessage()], 401);
+        }
+
+        $guard = data_get($exception->guards(), 0); // Reemplaza array_get con data_get
+        switch ($guard) {
+            case 'admin':
+                $login = 'admin.login';
+                break;
+            case 'web':
+                $login = 'login';
+                break;
+            default:
+                $login = 'login';
+        }
+
+        // Redirige al usuario invitado (no autenticado) a la ruta de inicio de sesión correspondiente al guardia
+        return redirect()->guest(route($login));
     }
 }
