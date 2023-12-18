9dd1978c2740da12614b1873c2eea2682fa0ecb5
Gabriel Castro <gabriel.programador.ads@gmail.com>
2023-12-18 11:27:31 -0300

    att

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,18 +12,35 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-Route::get('/', 'FirstController@index');
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/produtos', 'ProdutoControlador@index');
+// primeiro esse
 
-Route::get('/departamentos', 'DepartamentoControlador@index');
+//    // ==> acrescentar
+//    Route::get('/admin/login', 'Auth\AdminLoginController@index')->name('admin.login');
+//    Route::post('/admin/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+//    
+//    
+//    // ==> troquei de lugar
+//    Route::get('/admin', 'AdminController@index')->name('admin.dashboard');
 
-Route::get('/usuario', function(){
-    return view('usuario');
+
+// depois esse
+Route::prefix('/admin')->group(function() {
+    Route::get('/login', 'Auth\AdminLoginController@index')->name('admin.login');
+    Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+    Route::get('/', 'AdminController@index')->name('admin.dashboard');
 });
 
-Route::get('/admin', 'AdminController@index')->name('homeadmin');
+
+
+
+
+
+
